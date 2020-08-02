f546987be71f24dfb49a97594cf75fc1d8f0d57f
type59magictank <type59magictank@foxmail.com>
2020-08-02 11:06:09 +0800

    基础应用

---
+++ functionexp/Assets/enemyzig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyzig : enemy
+{
+
+    public override void move()//重写
+    {
+        Vector3 temppos = pos;
+        temppos.x = Mathf.Sin(Time.time * Mathf.PI * 2) * 4;
+        pos = temppos;
+        base.move();//调用父类方法
+    }
+
+}
---
+++ functionexp/Assets/movealong.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movealong : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        countithigher cih = this.gameObject.GetComponent<countithigher>();
+        //cih局部变量引用countithigher实例。GetComponent<countithigher>找到绑定的脚本组件
+        if (cih != null)
+        {
+            float tx = cih.currentnum / 10f;
+            Vector3 temploc = pos;
+            temploc.x = tx;
+            pos = temploc;
+        }
+    }
+
+    public Vector3 pos
+    {
+        get { return (this.transform.position); }
+        set { this.transform.position = value; }
+    }
+
+}
