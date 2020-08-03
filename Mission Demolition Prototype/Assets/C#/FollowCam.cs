656028dd6f9884cad7ba8dd95825865c60566720
type59magictank <type59magictank@foxmail.com>
2020-08-03 17:49:15 +0800

    8.3

---
+++ Mission Demolition Prototype/Assets/C#/FollowCam.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam S;
+
+    public float easing = 0.05f;
+
+    public bool ______________________________;
+
+    public GameObject poi;//兴趣点
+    public float camZ;
+    public Vector2 minXY=Vector2.zero;
+
+    private void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (poi == null) return;
+
+        Vector3 destination = poi.transform.position;
+        //兴趣点位置
+        destination.x = Mathf.Max(0, destination.x);
+        destination.y = Mathf.Max(0, destination.y);
+        //限定xy最小值
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+        //设置使得地面始终在
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        //位置插值
+        destination.z = camZ;
+        transform.position = destination;
+        //设置摄像机位置
+    }
+
+}
--- Mission Demolition Prototype/Assets/C#/Slingshot.cs
+++ Mission Demolition Prototype/Assets/C#/Slingshot.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour
 {
 
+    public GameObject prefabProjectile;
+    public float velocityMult = 4f;
+    //上面需要在检视面板设置
+    public bool ___________________________;
+
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +24,36 @@ public class Slingshot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!aimingMode) return;
         
+        Vector3 mousePos2D = Input.mousePosition;
+        //获取鼠标2d窗口位置
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        //转换为3d位置
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        //计算坐标差
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        //限制坐标差在碰撞器半径内
+
+        Vector3 proPos = launchPos + mouseDelta;
+        projectile.transform.position = proPos;
+        //移动位置
+        if (Input.GetMouseButtonUp(0))
+        {
+            //松开鼠标
+            aimingMode = false;
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+            projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+            FollowCam.S.poi = projectile;
+            projectile = null;
+        }
     }
 
     private void Awake()
@@ -24,6 +61,8 @@ public class Slingshot : MonoBehaviour
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+
+        launchPos = launchPointTrans.position;
 
     }
 
@@ -37,6 +76,18 @@ public class Slingshot : MonoBehaviour
     {
         //print("Slingshot:OnMouseExit");
         launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+        //范围内点击鼠标
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //实例化弹丸
+        projectile.transform.position = launchPos;
+        //初始位置
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        //设置属性
     }
 
 }
